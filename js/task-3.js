098f768c943f4c595cc6380f341b920f7c65c9be
Vladyslav Voronin <157734203+Vraznos@users.noreply.github.com>
2024-04-14 13:31:38 +0200

    js homework 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border){
-    const x = Number.parseFloat(content);
-    const y = (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
-    const result = x + y;
-    return result;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px")); 
+function checkForSpam(message){
+  let lowerMessage = message.toLowerCase();
+    if (lowerMessage.includes(`sale`) || lowerMessage.includes(`spam`)){
+        return true;
+    }else{
+        return false;
+            }
+    }
+    console.log(checkForSpam("Latest technology news")); // false
+    console.log(checkForSpam("JavaScript weekly newsletter")); // false
+    console.log(checkForSpam("Get best sale offers now!")); // true
+    console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+    console.log(checkForSpam("Trust me, this is not a spam message")); // true
+    console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+    console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
