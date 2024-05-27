5add3a96ae29fba22481126b7e6618422befce97
Vladyslav Voronin <157734203+Vraznos@users.noreply.github.com>
2024-05-27 14:43:31 +0200

    js-hw-08

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesLength = document.querySelectorAll('ul#categories > li.item');
-console.log(`Number of categories: ${categoriesLength.length}`);
-
-categoriesLength.forEach(category => {
-  const categoryName = category.querySelector(`h2`).textContent;
-  const categoryItemCount = category.querySelectorAll(`ul > li`).length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryItemCount}`);
-});
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById(`name-input`);
-const output = document.getElementById(`name-output`);
-input.addEventListener(`input`, ev => {
-  const inputResult = ev.target.value.trim();
-  output.textContent = inputResult || `Anonymous`;
-});
